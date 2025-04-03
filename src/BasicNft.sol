3389f647ebec548d0603b78973a6999ce48d606e
uzairnadeem201 <uzairnadeem201@gmail.com>
2025-04-04 02:30:13 +0500

    Setup Openzeppelin

---
+++ src/BasicNft.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNft is ERC721 {
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Dogie", "DOG") {
+        s_tokenCounter = 0;
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        return "";
+    }
+}

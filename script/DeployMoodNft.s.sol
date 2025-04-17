071a57441b50f8c743ff395489fd115fd4b1c855
uzairnadeem201 <uzairnadeem201@gmail.com>
2025-04-17 14:24:29 +0500

    NFT Mood is Changing on Chain

--- script/DeployMoodNft.s.sol
+++ script/DeployMoodNft.s.sol
@@ -1,13 +1,25 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.18;
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {MoodNft} from "../src/MoodNft.sol";
 import {console} from "forge-std/console.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract DeployMoodNft is Script {
-    function run() external returns (MoodNft) {}
+    function run() external returns (MoodNft) {
+        string memory sadSvg = vm.readFile("./img/sad.svg");
+        string memory happySvg = vm.readFile("./img/happy.svg");
+        console.log("Sad SVG: ", sadSvg);
+        console.log("Happy SVG: ", happySvg);
+        vm.startBroadcast();
+        MoodNft moodNft = new MoodNft(
+            svgToImageURI(sadSvg),
+            svgToImageURI(happySvg)
+        );
+        vm.stopBroadcast();
+        return moodNft;
+    }
 
     function svgToImageURI(
         string memory svg
--- test/integrations/BasicNftTest.t.sol
+++ test/integrations/BasicNftTest.t.sol
@@ -1,8 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 import {Test} from "forge-std/Test.sol";
-import {BasicNft} from "../src/BasicNft.sol";
-import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
+import {BasicNft} from "../../src/BasicNft.sol";
+import {DeployBasicNft} from "../../script/DeployBasicNft.s.sol";
 
 contract BasicNftTest is Test {
     DeployBasicNft public deployer;
